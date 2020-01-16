d8552e64a0b64a631dee703f9d74a5c150818e48
shaGuar <shaGuar-svg@users.noreply.github.com>
2020-01-16 21:56:52 +0800

    1

---
+++ JS/QYXJ.js
@@ -0,0 +1,21 @@
+//  正则^https:\/\/commerce-api\.faceu\.mobi\/commerce\/v1\/subscription\/user_info
+// Mitm.commerce-api.faceu.mobi
+
+let obj = JSON.parse($response.body);
+obj ={
+"ret":"0",
+"errmsg":"Success",
+"systime":"",
+"data":{
+"flag":true,
+"start_time":1579094492,
+"end_time":3725012184,
+"is_first_subscribe":false,
+"is_cancel_subscribe":false,
+"uid":"4089778978429341",
+"subscribe_uid":"70851259049"
+}
+};
+$done({body: JSON.stringify(obj)});
+
+//轻颜相机
