715033834591e4ee03e34dc5094aa25f7e0e6ed8
shaGuar <shaGuar-svg@users.noreply.github.com>
2020-02-29 10:10:27 +0800

    nextdns

---
+++ Conf/rules/nextdns.js
@@ -0,0 +1,11 @@
+//Script event auto linked ipv4 nextdns: network-change
+
+$httpClient.get('https://link-ip.nextdns.io/d49521/c274ce33ad2401ca', function(error, response, data){
+  if (error) {
+$notification.post('NEXT DNS ', 'Internet error','');
+    $done({});
+  } else {
+$notification.post('NEXT DNS ', 'IPv4 (with linked IP)', 'ip :' + data);
+    $done({});
+  }
+});
