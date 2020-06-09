55581c8880756e0524edc1e60c27628d32f4f59c
shawnleong <shaguar09@outlook.com>
2020-06-09 10:12:26 +0800

    fix bybutter.js

--- Script/Unlock/bybutter.js
+++ Script/Unlock/bybutter.js
@@ -1,32 +1,15 @@
-/*
-[rewrite_local]
-^https://api4\.bybutter\.com(/v4/users/me|/v4/shop/.+) url script-response-body bybutter.js
-[mitm]
-hostname = api4.bybutter.com,
-*/
+/*黄油相机PRO
 
-if ($request.url.indexOf("/v4/users/me") != -1) {
-    re('memberships":\\[\\]', 'memberships":[{"endAt":1650842277,"id":"1","name":"普通会员","ownership":"temporary","startAt":1587425677,"usageType":"unlimited"}]')
-    }
-    
-    if ($request.url.indexOf("/v4/shop/") != -1) {
-    re('ownership":"\\w+@usageType":"\\w+@remark":"[^"]+', 'ownership":"free@usageType":"unlimited@remark":"使用期限：永久')
-    }
-    
-    
-    function re() {
-      var body = $response.body;
-      if (arguments[0].includes("@")) {
-       var regs = arguments[0].split("@");
-       var strs = arguments[1].split("@");
-       for (i = 0;i < regs.length;i++) {
-        var reg = new RegExp(regs[i],"g");
-        body = body.replace(reg, strs[i]);
-      }
-     }
-      else {
-       var reg = new RegExp(arguments[0],"g");
-       body = body.replace(reg, arguments[1]);
-     }
-      $done(body);
-    }
+[Script]
+http-response https?:\/\/.*\.bybutter\.com\/(v4\/shop\/|v4\/users\/me|v4\/shop\/shelves|v4\/editor\/template\/recommended|v4\/search\/products|v4\/template-square\/) requires-body=1,max-size=0,script-path= https://raw.githubusercontent.com/Gx3dong/Surge/master/JS/hyxj.js
+
+[MITM]
+hostname = *.bybutter.com
+*/ 
+
+
+body = $response.body.replace(/\"ownership\":membership/g, "\"ownership\":free").replace(/\"enable\":false/g, "\"enable\":true").replace(/\"memberships\":\[\]/g, "\"memberships\":[{\"endAt\":1650842277,\"id\":\"1\",\"name\":\"普通会员\",\"ownership\":\"temporary\",\"startAt\":1587425677,\"usageType\":\"unlimited\"}]").replace(/\"trial\":false/g, "\"trial\":true").replace(/\"colorful\":false/g, "\"colorful\":true").replace(/\"masking\":false/g, "\"masking\":true").replace(/"remark":"[^""]+"/g, '\"remark\":\"使用期限：永久"').replace(/\"ownership\":builtin/g, "\"ownership\":free").replace(/\"ownership\":promotion/g, "\"ownership\":free").replace(/\"usageType\":view/g, "\"usageType\":unlimited").replace(/\"ownership\":membership/g, "\"ownership\":free")
+
+
+
+$done({body});
