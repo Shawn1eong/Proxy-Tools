d563a51732e7eb44f9b600a86c141788f072582d
shaguar-svg <shaguar09@outlook.com>
2021-01-23 13:51:53 +0800

    update

--- Script/Crack/cpdq.js
+++ Script/Crack/cpdq.js
@@ -2,10 +2,7 @@
 菜谱大全 unlock vip
 
 #菜谱大全解锁vip
-https?:\/\/api\.jiaonizuocai\.com url script-response-body https://raw.githubusercontent.com/photonmang/quantumultX/master/cpdq.js
-
-#Surge4.0
-^https?:\/\/api\.jiaonizuocai\.com requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/shaGuar-svg/Proxy-Tools/master/JS/Unlock/cpdq.js
+https?:\/\/api\.jiaonizuocai\.com url script-response-body cpdq.js
 
 
 MITM = api.jiaonizuocai.com
@@ -26,14 +23,10 @@ if (url.indexOf(vip) != -1) {
    body = JSON.stringify(obj);
    }
    
-
 if (url.indexOf(dish) != -1) {
-  power = Object.keys(obj["data"]["power"]);
-  if (power.length != 0) {
-    obj["data"]["power"]["detail"]["video"]["common"]["isShow"] = "0";
-  }
-  body = JSON.stringify(obj);
-}
+obj["data"]["power"]["detail"]["video"]["common"]["isShow"] = "0";
+     body = JSON.stringify(obj);
+     }
 
 if (url.indexOf(sb) != -1) {
 	obj["data"]["is_vip"] = "2";
