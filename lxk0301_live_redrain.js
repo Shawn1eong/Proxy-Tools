f45128160eb6847db1f95e4848a72ab2f47a5fc6
ShaGuar-svg <shaguar09@outlook.com>
2021-04-21 17:06:35 +0800

    update

--- lxk0301_live_redrain.js
+++ lxk0301_live_redrain.js
@@ -11,22 +11,22 @@ let bodyList = {
   }
 }
 let ids = {
-  '8': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '9': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '10': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '11': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '12': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '13': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '14': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '15': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '16': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '17': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '18': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '19': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '20': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '21': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '22': 'RRA4RhWMc159kA62qLbaEa88evE7owb',
-  '23': 'RRA4RhWMc159kA62qLbaEa88evE7owb'
+  '8': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '9': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '10': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '11': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '12': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '13': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '14': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '15': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '16': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '17': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '18': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '19': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '20': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '21': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '22': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD,
+  '23': 'RRA4Kr8hch51J49qasBvEv8Agf8chAD
 }
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
