5b010249922d677b495c322f5011077952a0249f
shawn1eong <s4awnleong@outlook.com>
2021-05-07 19:10:28 +0800

    update

---
+++ Module/Class/Zhihu/zhihu_topstory_recommend.js
@@ -0,0 +1,6 @@
+let body = $response.body;
+body = body.replace(/("id")\s*:\s*(\d{15,})/g, "\$1:\"$2\"");  // 大整数会有精度损失
+body = JSON.parse(body);
+body['data'] = body['data'].filter(element => !(element.hasOwnProperty('ad')));
+body = JSON.stringify(body);
+$done({body});
