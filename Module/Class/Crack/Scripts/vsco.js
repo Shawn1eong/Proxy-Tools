9274074081a49f32e8ebb7fc166db295c019359d
shaGuar-svg <82190871+Shawn1eong@users.noreply.github.com>
2021-05-23 19:09:13 +0800

    Update vsco.js

--- Module/Class/Crack/Scripts/vsco.js
+++ Module/Class/Crack/Scripts/vsco.js
@@ -2,34 +2,77 @@
 VSCO unlock vip
 
 QX:
-^https?:\/\/vsco\.co\/api\/subscriptions\/2.1\/user-subscriptions\/ url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+^https?:\/\/vsco\.co\/api\/subscriptions\/2.1\/user-subscriptions\/ url script-response-body https://raw.githubusercontent.com/Shawn1eong/Proxy-Tools/main/Module/Class/Crack/Scripts/vsco.js
 
 Surge4：
-http-response ^https?:\/\/vsco\.co\/api\/subscriptions\/2.1\/user-subscriptions\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/vsco.js
+http-response ^https?:\/\/vsco\.co\/api\/subscriptions\/2.1\/user-subscriptions\/ requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/Shawn1eong/Proxy-Tools/main/Module/Class/Crack/Scripts/vsco.js
 
 Surge & QX MITM = vsco.co
 */
 
-var body = $response.body;
-var url = $request.url;
-
-const path1 = '/api/subscriptions/2.1/user-subscriptions/';
-
-let obj = JSON.parse(body);
-
-if (url.indexOf(path1) != -1) {
-	obj.user_subscription["expires_on_sec"] = 1655536094;
-	obj.user_subscription["expired"] = false;
-	obj.user_subscription["payment_type"] = 2;
-	obj.user_subscription["is_trial_period"] = true;
-	obj.user_subscription["starts_on_sec"] = 1560831070;
-	obj.user_subscription["is_active"] = true;
-	obj.user_subscription["auto_renew"] = true;
-	obj.user_subscription["last_verified_sec"] = 1560831070;
-	obj.user_subscription["subscription_code"] = "VSCOANNUAL";
-	obj.user_subscription["user_id"] = 54624336;
-	obj.user_subscription["source"] = 1;
-	body = JSON.stringify(obj);  
- }
-
-$done({body});
+var __encode = 'jsjiami.com',
+	_a = {},
+	_0xb483 = ["_decode", "http://www.sojson.com/javascriptobfuscator.html"];
+(function(_0xd642x1) {
+	_0xd642x1[_0xb483[0]] = _0xb483[1]
+})(_a);
+var __Oxbf4a0 = ["2020-06-05T11:54:41Z", "2030-02-18T07:52:54Z", "com.circles.fin.premium.yearly", "2020-02-11T07:52:54Z", "2020-05-29T07:59:41Z", "2020-06-05T11:46:28Z", "RbhyxwVVYSgnnUEtme2444PjccJ3", "5", "2020-05-29T07:47:32Z", "2020-02-11T07:52:55Z", "normal", "app_store", "stringify", "undefined", "log", "删除", "版本号，js会定", "期弹窗，", "还请支持我们的工作", "jsjia", "mi.com"];
+obj = {
+	"request_date": __Oxbf4a0[0x0],
+	"request_date_ms": 1591358081473,
+	"subscriber": {
+		"entitlements": {
+			"membership": {
+				"expires_date": __Oxbf4a0[0x1],
+				"product_identifier": __Oxbf4a0[0x2],
+				"purchase_date": __Oxbf4a0[0x3]
+			}
+		},
+		"first_seen": __Oxbf4a0[0x4],
+		"last_seen": __Oxbf4a0[0x5],
+		"management_url": null,
+		"non_subscriptions": {},
+		"original_app_user_id": __Oxbf4a0[0x6],
+		"original_application_version": __Oxbf4a0[0x7],
+		"original_purchase_date": __Oxbf4a0[0x8],
+		"other_purchases": {},
+		"subscriptions": {
+			"com.circles.fin.premium.yearly": {
+				"billing_issues_detected_at": null,
+				"expires_date": __Oxbf4a0[0x1],
+				"is_sandbox": false,
+				"original_purchase_date": __Oxbf4a0[0x9],
+				"period_type": __Oxbf4a0[0xa],
+				"purchase_date": __Oxbf4a0[0x3],
+				"store": __Oxbf4a0[0xb],
+				"unsubscribe_detected_at": null
+			}
+		}
+	}
+};
+$done({
+	body: JSON[__Oxbf4a0[0xc]](obj)
+});;;
+(function(_0x9753x1, _0x9753x2, _0x9753x3, _0x9753x4, _0x9753x5, _0x9753x6) {
+	_0x9753x6 = __Oxbf4a0[0xd];
+	_0x9753x4 = function(_0x9753x7) {
+		if (typeof alert !== _0x9753x6) {
+			alert(_0x9753x7)
+		};
+		if (typeof console !== _0x9753x6) {
+			console[__Oxbf4a0[0xe]](_0x9753x7)
+		}
+	};
+	_0x9753x3 = function(_0x9753x8, _0x9753x1) {
+		return _0x9753x8 + _0x9753x1
+	};
+	_0x9753x5 = _0x9753x3(__Oxbf4a0[0xf], _0x9753x3(_0x9753x3(__Oxbf4a0[0x10], __Oxbf4a0[0x11]), __Oxbf4a0[0x12]));
+	try {
+		_0x9753x1 = __encode;
+		if (!(typeof _0x9753x1 !== _0x9753x6 && _0x9753x1 === _0x9753x3(__Oxbf4a0[0x13], __Oxbf4a0[0x14]))) {
+			_0x9753x4(_0x9753x5)
+		}
+	} catch (e) {
+		_0x9753x4(_0x9753x5)
+	}
+})({})
