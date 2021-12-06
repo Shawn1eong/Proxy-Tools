5d2a98f388b25d11680c3738b1f03cdfab41a99d
shawn1eong <s4awnleong@outlook.com>
2021-12-06 12:46:38 +0800

    update

--- Script/Adblock/WeChat.js
+++ Script/Adblock/WeChat.js
@@ -1,15 +1,28 @@
 /*
+微信 去除公众号文章底部广告
+
+***************************
+QuantumultX:
+
+[rewrite_local]
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+
+[mitm]
+hostname = mp.weixin.qq.com
+
+***************************
+Surge4 or Loon:
+
 [Script]
-http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://Choler.github.io/Surge/Script/WeChat.js
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
 
 [MITM]
 hostname = mp.weixin.qq.com
-*/
 
-var data = {
-  body: "{}",
-  headers: {
-    "Content-Type": "application/json"
-  }
-};
-$done({response: data});
+**************************/
+
+var obj = JSON.parse($response.body);
+obj.advertisement_num = 0;
+obj.advertisement_info = [];
+delete obj.appid;
+$done({body: JSON.stringify(obj)}); 
