d1a327c7637282f677211bc78a044b41a96083af
nikkwo <nikkwoo@outlook.com>
2022-12-07 23:10:01 +0800

    update
    fix

--- Script/Crack/Alex/Adguard.js
+++
@@ -1,4 +0,0 @@
-var obj = JSON.parse($response.body);
-
-obj = {"products":[{"product_id":"com.adguard.lifetimePurchase","premium_status":"ACTIVE"}]}
-$done({body: JSON.stringify(obj)});
--- Script/Crack/Alex/mubu.js
+++
@@ -1,9 +0,0 @@
-/*
-幕布解锁Pro
-https://mubu.com/api/app/user/info
-*/
-
-let obj = JSON.parse($response.body);
-
-obj.data.vipEndDate = "20291231"
-$done({body: JSON.stringify(obj)});
