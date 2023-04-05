9181a860a0ad0e260973ecc8a46577bd8d0db67b
Shawnleong <82190871+Shawn1eong@users.noreply.github.com>
2023-04-05 11:22:48 +0800

    Delete Demo directory

--- Demo/demo-2.js
+++
@@ -1,9 +0,0 @@
-var obj = JSON.parse($response.body); 
-// 可以合并一句带过
-
-obj = {"subscribed":true,"subscriptionStatus":"YEARLY_SUBSCRIBED","eligibleForFreeTrial":true}
-// 有需要全部替换的情况，只需让用户在这里如此操作即可
-
-$done({body:JSON.stringify(obj)});
-
-// 也一句带过
